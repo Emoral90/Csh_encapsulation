ece266dd502134f80bb6464089bef69fba0b7766
gasolina90 <moralerick90@gmail.com>
2023-01-31 16:00:24 -0700

    modified file

--- prep_encap/Program.cs
+++ prep_encap/Program.cs
@@ -1,21 +1,21 @@
 ﻿// namespace Fraction;
 
 public class Program{
-    // Global variables
-    private static double NUMERATOR;
-    private static double DENOMINATOR;
-    
+    // If done well, there will be no need for global variables
+
     // Entry point for program
     static void Main(){
         // Constructors
         Fraction fraction = new Fraction();
 
-        Program.NUMERATOR = fraction.set_numerator(4);
-        Program.DENOMINATOR = fraction.set_denominator(5);
+        // assign new values to numerator and denominator from Fraction.cs
+        fraction.set_numerator(4);
+        fraction.set_denominator(5);
 
+        // print newly assigned values of numerator and denominator from Fraction.cs
         Console.WriteLine();
-        Console.WriteLine($"Private variable NUMERATOR: {Program.NUMERATOR}");
-        Console.WriteLine($"Private variable DENOMINATOR: {Program.DENOMINATOR}");
+        Console.WriteLine($"Private variable NUMERATOR: {fraction.get_numerator()}");
+        Console.WriteLine($"Private variable DENOMINATOR: {fraction.get_denominator()}");
         Console.WriteLine();
     }
 }
